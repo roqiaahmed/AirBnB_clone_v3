e70bd13599a07b5945363299597d0f34f36f6552
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-27 21:47:45 +0200

    api

---
+++ api/v1/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+@app.teardown_appcontext
+def teardown_db(self):
+    """Closes the storage"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="HBNB_API_HOST", port="HBNB_API_PORT", threaded=True)
