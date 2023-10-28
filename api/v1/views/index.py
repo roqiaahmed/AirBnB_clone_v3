2360384a23a57ca5381af7e976c9dba910b13a34
BasmaMagdy <basma.magdy998@gmail.com>
2023-10-28 12:17:50 -0700

    go

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,17 +3,6 @@
 from models import storage
 from flask import jsonify
 from api.v1.views import app_views
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
-
-#classes = {"users": "User", "places": "Place", "states": "State",
-#           "cities": "City", "amenities": "Amenity",
-#           "reviews": "Review"}
 
 
 @app_views.route('/status', methods=['GET'])
@@ -33,6 +22,4 @@ def count():
         'states' : storage.count('State'),
         'users' : storage.count('User')
     }
-#    for cls in classes:
-#        count_dict[cls] = storage.count(classes[cls])
     return jsonify(count_dict)
