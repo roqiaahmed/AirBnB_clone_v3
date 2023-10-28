e7033757222d8668c25eb5990ac7a5817c7d71db
BasmaMagdy <basma.magdy998@gmail.com>
2023-10-28 12:21:00 -0700

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3xx
-'''api status'''
-from models import storage
+'''api count'''
 from flask import jsonify
+from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
-    ''' routes to status page '''
-    return jsonify({'status': 'OK'})
+    """ returns status """
+    status = {"status": "OK"}
+    return jsonify(status)
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats')
 def count():
-    '''retrieves the number of each objects by type'''
-    count_dict = {
-        'amenities' : storage.count('Amenity'),
-        'cities' : storage.count('City'),
-        'places' : storage.count('Place'),
-        'reviews' : storage.count('Review'),
-        'states' : storage.count('State'),
-        'users' : storage.count('User')
-    }
-    return jsonify(count_dict)
+    """ returns number of each objects by type """
+    total = {}
+    classes = {"Amenity": "amenities",
+               "City": "cities",
+               "Place": "places",
+               "Review": "reviews",
+               "State": "states",
+               "User": "users"}
+    for cls in classes:
+        count = storage.count(cls)
+        total[classes.get(cls)] = count
+    return jsonify(total)
