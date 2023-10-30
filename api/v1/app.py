cc37a5caeb7500248f2cf118885c314c16c806ca
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-30 10:21:44 +0200

    update_app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+""" Module for app """
 from flask import Flask, make_response, jsonify
 from models import storage
 from os import environ
 from api.v1.views import app_views
+
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -18,6 +21,7 @@ def teardown_db(error):
 def page_not_found(error):
     """Returns a JSON-formatted 404 status code response"""
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST')
