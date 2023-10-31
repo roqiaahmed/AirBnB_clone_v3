0668b831d6fb0f57f0142f0d51c5ccf3382be2d6
BasmaMagdy <basma.magdy998@gmail.com>
2023-10-31 14:29:05 -0700

    go

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,55 +1,70 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  1 14:42:23 2020
-@authors: Robinson Montes
-          Mauricio Olarte
-"""
-from flask import Blueprint, jsonify, request, abort
-from api.v1.views import app_views
+""" return dict repersantation of object """
 from models import storage
-from models.state import State
+from models.engine.db_storage import classes
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
 
 
-@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
-def states():
-    """Create a new view for State objects that handles all default
-    RestFul API actions.
-    """
-    if request.method == 'GET':
-        return jsonify([val.to_dict() for val in storage.all('State')
-                        .values()])
-    elif request.method == 'POST':
-        post = request.get_json()
-        if post is None or type(post) != dict:
-            return jsonify({'error': 'Not a JSON'}), 400
-        elif post.get('name') is None:
-            return jsonify({'error': 'Missing name'}), 400
-        new_state = State(**post)
-        new_state.save()
-        return jsonify(new_state.to_dict()), 201
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
+def state_get():
+    result = []
+    """ get all the objects from state """
+    for i in storage.all("State").values():
+        result.append(i.to_dict())
+    return jsonify(result)
 
 
-@app_views.route('/states/<string:state_id>',
-                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
-def get_state_id(state_id):
-    """Retrieves a state object with a specific id"""
-    state = storage.get('State', state_id)
-    if state is None:
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
+def state_specific(state_id):
+    """ get the specific object from state """
+    for i in storage.all("State").values():
+        if i.id == state_id:
+            return i.to_dict()
+    abort(404)
+
+
+@app_views.route("/states/<state_id>",
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def state_specific_delete(state_id):
+    """ delete the inputed object from state """
+    task = [task for task in storage.all(
+        "State").values() if task.id == state_id]
+    if len(task) == 0:
         abort(404)
-    elif request.method == 'GET':
-        return jsonify(state.to_dict())
-    elif request.method == 'DELETE':
-        state = storage.get('State', state_id)
-        storage.delete(state)
-        storage.save()
-        return jsonify({}), 200
-    elif request.method == 'PUT':
-        put = request.get_json()
-        if put is None or type(put) != dict:
-            return jsonify({'error': 'Not a JSON'}), 400
-        for key, value in put.items():
-            if key not in ['id', 'created_at', 'updated_at']:
-                setattr(state, key, value)
-                storage.save()
-        return jsonify(state.to_dict()), 200
+    storage.delete(task[0])
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/states", methods=['POST'], strict_slashes=False)
+def state_specific_post():
+    """ post the inputed object from state """
+    if not request.json:
+        return make_response("Not a JSON", 400)
+    if 'name' not in request.json:
+        return make_response("Missing name", 400)
+    obj = classes["State"]
+    new_inst = obj(**request.json)
+    new_inst.save()
+    return new_inst.to_dict(), 201
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
+def state_specific_put(state_id):
+    """ update the specific object from state """
+    instance = None
+    if not request.json:
+        return make_response("Not a JSON", 400)
+    check = ["id", "created_at", "updated_at"]
+    for i in storage.all("State").values():
+        if i.id == state_id:
+            instance = i
+            for key, value in request.json.items():
+                if key not in check:
+                    setattr(i, key, value)
+                    i.save()
+    if not instance:
+        abort(404)
+    return instance.to_dict(), 200
